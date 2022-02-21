7807c740f2709d88066cf3437d018f347a879c93
FernandoTorresDev <fernandotorrescandeia@gmail.com>
2022-02-21 19:26:22 -0300

    added files

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import Footer from "../comps/Footer";
-import Navbar from "../comps/Navbar";
-import styles from "../styles/Home.module.css";
-
-export default function Home() {
+const About = () => {
   return (
     <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
+      <h1>About</h1>
+      <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
         quibusdam voluptatum. Repellendus totam quidem qui, iusto, alias earum
         aut doloribus veritatis, accusantium nemo odit esse deleniti voluptatem
@@ -18,9 +11,8 @@ export default function Home() {
         Illum sunt facere, ipsa distinctio eligendi modi officiis dolorem
         dolorum natus repudiandae quasi cum nam nemo vero?
       </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
     </div>
   );
-}
+};
+
+export default About;
---
+++ pages/ninjas/ninja.js
@@ -0,0 +1,9 @@
+const Ninjas = () => {
+  return (
+    <div>
+      <h1>Ninjas inside ninja</h1>
+    </div>
+  );
+};
+
+export default Ninjas;
